0d117fe5f9cd277f2fab3cc4c0dc4bd54a820000
george.butnaru <geo.butnaru@gmail.com>
2023-02-18 21:55:58 +0200

    validation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import Label, SubmitField
-from wtforms.fields import StringField
-
+from wtforms import Label, SubmitField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Email, Length
 app = Flask(__name__)
 app.secret_key = "some secret string"
 
@@ -12,15 +11,17 @@ def home():
 
 
 class LoginForm(FlaskForm):
+
     email_label = Label(field_id="email", text="Email")
-    email = StringField(label='email')
+    email = EmailField(label='email', validators=[DataRequired(), Email()])
     password_label = Label(field_id="password", text="Password")
-    password = StringField(label='password')
-    button = SubmitField()
+    password = PasswordField (label='password', validators=[DataRequired(), Length(min=8)])
+    button = SubmitField("Log in")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 if __name__ == '__main__':
